4be7abbdab57e89c7bd0101baef0b7601a99d535
TrSkorpio <131482954+TrSkorpio@users.noreply.github.com>
2024-11-19 23:01:39 +0300

    Update aht10.cpp

--- esphome/components/aht10/aht10.cpp
+++ esphome/components/aht10/aht10.cpp
@@ -25,8 +25,8 @@ static const uint8_t AHT20_INITIALIZE_CMD[] = {0xBE, 0x08, 0x00};
 static const uint8_t AHT10_MEASURE_CMD[] = {0xAC, 0x33, 0x00};
 static const uint8_t AHT10_SOFTRESET_CMD[] = {0xBA};
 
-static const uint8_t AHT10_DEFAULT_DELAY = 5;     // ms, for initialization and temperature measurement
-static const uint8_t AHT10_READ_DELAY = 80;       // ms, time to wait for conversion result
+static const uint8_t AHT10_DEFAULT_DELAY = 75;     // ms, for initialization and temperature measurement
+static const uint8_t AHT10_READ_DELAY = 75;       // ms, time to wait for conversion result
 static const uint8_t AHT10_SOFTRESET_DELAY = 30;  // ms
 
 static const uint8_t AHT10_ATTEMPTS = 3;  // safety margin, normally 3 attempts are enough: 3*30=90ms
